1c037d7b7e7ef7f88871d96d5eb249d5abcf0dfa
acfidelino <acfidelino@student.apc.edu.ph>
2023-03-02 10:31:32 +0800

    Code Igniter: News Section Task

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -29,9 +29,18 @@ $routes->set404Override();
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
+
+/*$routes->get('/', 'Home::index');
 use App\Controllers\Pages;
 
+$routes->get('pages', [Pages::class, 'index']);
+$routes->get('(:segment)', [Pages::class, 'view']);*/
+
+use App\Controllers\News;
+use App\Controllers\Pages;
+
+$routes->get('news/(:segment)', [News::class, 'view']);
+$routes->get('news', [News::class, 'index']);
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('(:segment)', [Pages::class, 'view']);
 
