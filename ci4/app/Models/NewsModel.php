3f207a566aaece375cce569187f8f3fd1fd95715
acfidelino <acfidelino@student.apc.edu.ph>
2023-03-02 10:38:13 +0800

    Updated NewsModel.php

--- ci4/app/Models/NewsModel.php
+++ ci4/app/Models/NewsModel.php
@@ -7,7 +7,8 @@ use CodeIgniter\Model;
 class NewsModel extends Model
 {
     protected $table = 'news';
-    
+    protected $table = 'acfidelino_news';
+
     public function getNews($slug = false)
     {
         if ($slug === false) {
